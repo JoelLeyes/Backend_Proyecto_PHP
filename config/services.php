245f7ea9acd096fcad7473a8f669ef46fb2fb571
Alvaro Garula <garulaalvaro7@gmail.com>
2026-05-24 20:54:14 -0300

    areglo de Autenticación con redes sociales (OAuth)

--- config/services.php
+++ config/services.php
@@ -54,19 +54,19 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID', ''),
         'client_secret' => env('GOOGLE_CLIENT_SECRET', ''),
-        'redirect'      => env('GOOGLE_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/auth/google/callback'),
+        'redirect'      => env('GOOGLE_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/api/auth/google/callback'),
     ],
 
     'github' => [
         'client_id'     => env('GITHUB_CLIENT_ID', ''),
         'client_secret' => env('GITHUB_CLIENT_SECRET', ''),
-        'redirect'      => env('GITHUB_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/auth/github/callback'),
+        'redirect'      => env('GITHUB_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/api/auth/github/callback'),
     ],
 
     'facebook' => [
         'client_id'     => env('FACEBOOK_CLIENT_ID', ''),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET', ''),
-        'redirect'      => env('FACEBOOK_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/auth/facebook/callback'),
+        'redirect'      => env('FACEBOOK_REDIRECT_URI', rtrim(env('APP_URL', 'http://localhost:8000'), '/').'/api/auth/facebook/callback'),
     ],
 
     'livekit' => [
