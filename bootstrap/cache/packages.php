0c502ae7615bb29bcd600378ce1895156034afda
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-06-04 20:02:35 -0300

    FUNKA?

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,9 +1,28 @@
 <?php return array (
+  'laravel/reverb' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Reverb\\ApplicationManagerServiceProvider',
+      1 => 'Laravel\\Reverb\\ReverbServiceProvider',
+    ),
+  ),
   'laravel/sanctum' => 
   array (
     'providers' => 
     array (
       0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
     ),
   ),
   'nesbot/carbon' => 
